bd348006cdefb7501457d712ff7c7abb66580cdf
unknown <adityakhandagale69@gmail.com>
2024-11-27 23:11:59 +0530

    commiting on 27th of nov 2024

--- server/server.js
+++ server/server.js
@@ -1,24 +1,37 @@
 const http = require("http");
-const fss = require("fs");
-const express = require("express");
+const fs = require("fs");
+const url = require("url");
 
-const server = http.createServer((req,res)=> {
-    //console.log(req.url);
-    
-    //fss.appendFile("/text.txt","UTF-8",log)
-    const log = `${Date.now()}: ${req.url}: new request recived \n`
-    fss.appendFile("text.txt",log, (err, data) => {
-        switch (req.url) {
-            case "/":
-                res.end("homepage")
-                break;
+const server = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") return res.end();
 
-            default:
-                res.end("404")
-                break;
-        }
-    })
-    //res.end("cndwjdj");
+    // Parse the URL for routing
+    const myurl = url.parse(req.url, true);
+    console.log(myurl.query);
 
+    // Handle routing
+    switch (myurl.pathname) {
+        case "/":
+            res.end("homepage");
+            break;
+        case "/about":
+            res.end("vuerw9vijeqvojeivh-eiojvdahvbvye79qvhgidbvuehn");
+            break;
+
+        case "/search":
+            const result = myurl.query.search_query;
+            res.end("here is ur result " + result)
+            break;
+        default:
+            res.end("404");
+            break;
+    }
+
+    // Log the request independently
+    const log = `${Date.now()}: ${req.url}: new request received \n`;
+    fs.appendFile("text.txt", log, (err) => {
+        if (err) console.error("Failed to log request:", err);
+    });
 });
-server.listen(8000, ()=> console.log("server started"));
+
+server.listen(8000, () => console.log("Server started on port 8000"));
